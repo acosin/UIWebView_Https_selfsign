9fd04996ce5477116693f218369a8c5ad97e4a64
sin <sin@macbook-pro.ztgame.com>
2018-06-05 14:32:23 +0800

    解决UIWebView加载自签名的https页面，出现白屏的问题

---
+++ zt2zs/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  zt2zs
+//
+//  Created by sin on 2018/6/5.
+//  Copyright © 2018年 sin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
